c938dfad85e2638e48a9bbe0fde4299ebf239921
Alexander Winter <a.w1nter@hotmail.com>
2023-09-19 14:46:46 -0400

    Trying to combine UI and decodable examples

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,433 @@
+//! MiniSynth
+
+use bevy::audio::AddAudioSource;
+use bevy::audio::AudioPlugin;
+use bevy::audio::Source;
 use bevy::prelude::*;
+use bevy::reflect::TypePath;
+use bevy::utils::Duration;
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+#[derive(Asset, TypePath)]
+struct SineAudio {
+    frequency: f32,
 }
 
+use bevy::{
+    a11y::{
+        accesskit::{NodeBuilder, Role},
+        AccessibilityNode,
+    },
+    input::mouse::{MouseScrollUnit, MouseWheel},
+    prelude::*,
+    winit::WinitSettings,
+};
 
-fn hello_world() {
-    println!("hello world!");
+
+#[derive(Component, Default)]
+struct ScrollingList {
+    position: f32,
 }
 
+// This decoder is responsible for playing the audio,
+// and so stores data about the audio being played.
+struct SineDecoder {
+    // how far along one period the wave is (between 0 and 1)
+    current_progress: f32,
+    // how much we move along the period every frame
+    progress_per_frame: f32,
+    // how long a period is
+    period: f32,
+    sample_rate: u32,
+}
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+impl SineDecoder {
+    fn new(frequency: f32) -> Self {
+        // standard sample rate for most recordings
+        let sample_rate = 44_100;
+        SineDecoder {
+            current_progress: 0.,
+            progress_per_frame: frequency / sample_rate as f32,
+            period: std::f32::consts::PI * 2.,
+            sample_rate,
+        }
     }
 }
 
-pub struct HelloPlugin;
+// The decoder must implement iterator so that it can implement `Decodable`.
+impl Iterator for SineDecoder {
+    type Item = f32;
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, greet_people));
-        
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current_progress += self.progress_per_frame;
+        // we loop back round to 0 to avoid floating point inaccuracies
+        self.current_progress %= 1.;
+        Some(f32::sin(self.period * self.current_progress))
     }
 }
+// `Source` is what allows the audio source to be played by bevy.
+// This trait provides information on the audio.
+impl Source for SineDecoder {
+    fn current_frame_len(&self) -> Option<usize> {
+        None
+    }
+
+    fn channels(&self) -> u16 {
+        1
+    }
+
+    fn sample_rate(&self) -> u32 {
+        self.sample_rate
+    }
+
+    fn total_duration(&self) -> Option<Duration> {
+        None
+    }
+}
+
+// Finally `Decodable` can be implemented for our `SineAudio`.
+impl Decodable for SineAudio {
+    type Decoder = SineDecoder;
+
+    type DecoderItem = <SineDecoder as Iterator>::Item;
+
+    fn decoder(&self) -> Self::Decoder {
+        SineDecoder::new(self.frequency)
+    }
+}
+
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, HelloPlugin))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(AudioPlugin {
+            global_volume: GlobalVolume::new(0.2),
+        }))
+        .add_audio_source::<SineAudio>()
+        // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
+        .insert_resource(WinitSettings::desktop_app())
+        .add_systems(Startup, setup)
+        .add_systems(Update, mouse_scroll)
         .run();
+}
+
+fn setup(mut assets: ResMut<Assets<SineAudio>>,
+         mut commands: Commands,
+         asset_server: Res<AssetServer>) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    // root node
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                justify_content: JustifyContent::SpaceBetween,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            // left vertical fill (border)
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Px(200.),
+                        border: UiRect::all(Val::Px(2.)),
+                        ..default()
+                    },
+                    background_color: Color::rgb(0.65, 0.65, 0.65).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // left vertical fill (content)
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                width: Val::Percent(100.),
+                                ..default()
+                            },
+                            background_color: Color::rgb(0.15, 0.15, 0.15).into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            // text
+                            parent.spawn((
+                                TextBundle::from_section(
+                                    "Text Example",
+                                    TextStyle {
+                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                        font_size: 30.0,
+                                        color: Color::WHITE,
+                                    },
+                                )
+                                .with_style(Style {
+                                    margin: UiRect::all(Val::Px(5.)),
+                                    ..default()
+                                }),
+                                // Because this is a distinct label widget and
+                                // not button/list item text, this is necessary
+                                // for accessibility to treat the text accordingly.
+                                Label,
+                            ));
+                        });
+                });
+            // right vertical fill
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Column,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        width: Val::Px(200.),
+                        ..default()
+                    },
+                    background_color: Color::rgb(0.15, 0.15, 0.15).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // Title
+                    parent.spawn((
+                        TextBundle::from_section(
+                            "Scrolling list",
+                            TextStyle {
+                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                font_size: 25.,
+                                color: Color::WHITE,
+                            },
+                        ),
+                        Label,
+                    ));
+                    // List with hidden overflow
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                flex_direction: FlexDirection::Column,
+                                align_self: AlignSelf::Stretch,
+                                height: Val::Percent(50.),
+                                overflow: Overflow::clip_y(),
+                                ..default()
+                            },
+                            background_color: Color::rgb(0.10, 0.10, 0.10).into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            // Moving panel
+                            parent
+                                .spawn((
+                                    NodeBundle {
+                                        style: Style {
+                                            flex_direction: FlexDirection::Column,
+                                            align_items: AlignItems::Center,
+                                            ..default()
+                                        },
+                                        ..default()
+                                    },
+                                    ScrollingList::default(),
+                                    AccessibilityNode(NodeBuilder::new(Role::List)),
+                                ))
+                                .with_children(|parent| {
+                                    // List items
+                                    for i in 0..30 {
+                                        parent.spawn((
+                                            TextBundle::from_section(
+                                                format!("Item {i}"),
+                                                TextStyle {
+                                                    font: asset_server
+                                                        .load("fonts/FiraSans-Bold.ttf"),
+                                                    font_size: 20.,
+                                                    color: Color::WHITE,
+                                                },
+                                            ),
+                                            Label,
+                                            AccessibilityNode(NodeBuilder::new(Role::ListItem)),
+                                        ));
+                                    }
+                                });
+                        });
+                });
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Px(200.0),
+                        height: Val::Px(200.0),
+                        position_type: PositionType::Absolute,
+                        left: Val::Px(210.),
+                        bottom: Val::Px(10.),
+                        border: UiRect::all(Val::Px(20.)),
+                        ..default()
+                    },
+                    border_color: Color::GREEN.into(),
+                    background_color: Color::rgb(0.4, 0.4, 1.).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(NodeBundle {
+                        style: Style {
+                            width: Val::Percent(100.0),
+                            height: Val::Percent(100.0),
+                            ..default()
+                        },
+                        background_color: Color::rgb(0.8, 0.8, 1.).into(),
+                        ..default()
+                    });
+                });
+            // render order test: reddest in the back, whitest in the front (flex center)
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        position_type: PositionType::Absolute,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                width: Val::Px(100.0),
+                                height: Val::Px(100.0),
+                                ..default()
+                            },
+                            background_color: Color::rgb(1.0, 0.0, 0.).into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            parent.spawn(NodeBundle {
+                                style: Style {
+                                    // Take the size of the parent node.
+                                    width: Val::Percent(100.0),
+                                    height: Val::Percent(100.0),
+                                    position_type: PositionType::Absolute,
+                                    left: Val::Px(20.),
+                                    bottom: Val::Px(20.),
+                                    ..default()
+                                },
+                                background_color: Color::rgb(1.0, 0.3, 0.3).into(),
+                                ..default()
+                            });
+                            parent.spawn(NodeBundle {
+                                style: Style {
+                                    width: Val::Percent(100.0),
+                                    height: Val::Percent(100.0),
+                                    position_type: PositionType::Absolute,
+                                    left: Val::Px(40.),
+                                    bottom: Val::Px(40.),
+                                    ..default()
+                                },
+                                background_color: Color::rgb(1.0, 0.5, 0.5).into(),
+                                ..default()
+                            });
+                            parent.spawn(NodeBundle {
+                                style: Style {
+                                    width: Val::Percent(100.0),
+                                    height: Val::Percent(100.0),
+                                    position_type: PositionType::Absolute,
+                                    left: Val::Px(60.),
+                                    bottom: Val::Px(60.),
+                                    ..default()
+                                },
+                                background_color: Color::rgb(1.0, 0.7, 0.7).into(),
+                                ..default()
+                            });
+                            // alpha test
+                            parent.spawn(NodeBundle {
+                                style: Style {
+                                    width: Val::Percent(100.0),
+                                    height: Val::Percent(100.0),
+                                    position_type: PositionType::Absolute,
+                                    left: Val::Px(80.),
+                                    bottom: Val::Px(80.),
+                                    ..default()
+                                },
+                                background_color: Color::rgba(1.0, 0.9, 0.9, 0.4).into(),
+                                ..default()
+                            });
+                        });
+                });
+            // bevy logo (flex center)
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        position_type: PositionType::Absolute,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::FlexStart,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // bevy logo (image)
+                    // A `NodeBundle` is used to display the logo the image as an `ImageBundle` can't automatically
+                    // size itself with a child node present.
+                    parent
+                        .spawn((
+                            NodeBundle {
+                                style: Style {
+                                    width: Val::Px(500.0),
+                                    height: Val::Px(125.0),
+                                    margin: UiRect::top(Val::VMin(5.)),
+                                    ..default()
+                                },
+                                // a `NodeBundle` is transparent by default, so to see the image we have to its color to `WHITE`
+                                background_color: Color::WHITE.into(),
+                                ..default()
+                            },
+                            UiImage::new(asset_server.load("branding/bevy_logo_dark_big.png")),
+                        ))
+                        .with_children(|parent| {
+                            // alt text
+                            // This UI node takes up no space in the layout and the `Text` component is used by the accessiblity module
+                            // and is not rendered.
+                            parent.spawn((
+                                NodeBundle {
+                                    style: Style {
+                                        display: Display::None,
+                                        ..Default::default()
+                                    },
+                                    ..Default::default()
+                                },
+                                Text::from_section("Bevy logo", TextStyle::default()),
+                            ));
+                        });
+                });
+        });
+
+    // add a `SineAudio` to the asset server so that it can be played
+    let audio_handle = assets.add(SineAudio {
+        frequency: 440., //this is the frequency of A4
+    });
+    commands.spawn(AudioSourceBundle {
+        source: audio_handle,
+        ..default()
+    });
+}
+
+fn mouse_scroll(
+    mut mouse_wheel_events: EventReader<MouseWheel>,
+    mut query_list: Query<(&mut ScrollingList, &mut Style, &Parent, &Node)>,
+    query_node: Query<&Node>,
+) {
+    for mouse_wheel_event in mouse_wheel_events.iter() {
+        for (mut scrolling_list, mut style, parent, list_node) in &mut query_list {
+            let items_height = list_node.size().y;
+            let container_height = query_node.get(parent.get()).unwrap().size().y;
+
+            let max_scroll = (items_height - container_height).max(0.);
+
+            let dy = match mouse_wheel_event.unit {
+                MouseScrollUnit::Line => mouse_wheel_event.y * 20.,
+                MouseScrollUnit::Pixel => mouse_wheel_event.y,
+            };
+
+            scrolling_list.position += dy;
+            scrolling_list.position = scrolling_list.position.clamp(-max_scroll, 0.);
+            style.top = Val::Px(scrolling_list.position);
+        }
+    }
 }
